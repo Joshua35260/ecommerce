bdc698735e5dbb5928721da966951d65fcc5f47f
Joshua <joshua35260@gmail.com>
2022-09-07 23:08:44 +0200

    e-shop only ui/ux, no backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import Categories from "./components/Categories";
+import CategoryItem from "./components/CategoryItem";
+import Footer from "./components/Footer";
+import Cart from "./screens/Cart";
 import Home from "./screens/Home";
+import Login from "./screens/Login";
+import Product from "./screens/Product";
+import ProductList from "./screens/ProductList";
+import Register from "./screens/Register";
 
 const App = () => {
-  return <Home />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/list" element={<ProductList />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 };
 export default App;
