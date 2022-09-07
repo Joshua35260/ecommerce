884d89c2c47ff38a27e81a28dfbcc146f41f471f
Joshua <joshua35260@gmail.com>
2022-09-07 08:53:09 +0200

    slider ok

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -4,6 +4,11 @@ const Container = styled.div`
   height: 30px;
   background-color: teal;
   color: white;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
 `;
 const Announcement = () => {
   return <Container>Super Deal! Free Shipping on Orders Over $50</Container>;
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE ! GET FLASH 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN SALE",
+    desc: "DON'T COMPROMISE ON STYLE ! GET FLASH 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE ! GET FLASH 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "SPRING SALE",
+    desc: "DON'T COMPROMISE ON STYLE ! GET FLASH 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "POPULAR SALE",
+    desc: "DON'T COMPROMISE ON STYLE ! GET FLASH 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
