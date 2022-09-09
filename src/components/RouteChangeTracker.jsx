70ac76062a72e7cd4c755d92a06b481969163a14
Joshua <joshua35260@gmail.com>
2022-09-09 12:40:24 +0200

    fix import name in routechangestracker

--- src/components/RouteChangeTracker.jsx
+++ src/components/RouteChangeTracker.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const GARoutesTracker = ({ history }) => {
   history.listen((location, action) => {
