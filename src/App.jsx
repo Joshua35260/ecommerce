ad85bb477b689d1ddf18a32e20d2b4f8c249ec23
Joshua <joshua35260@gmail.com>
2022-09-09 12:36:06 +0200

    with ga4 test

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,25 @@ import Login from "./screens/Login";
 import Product from "./screens/Product";
 import ProductList from "./screens/ProductList";
 import Register from "./screens/Register";
+import ReactGA from "react-ga4";
+import RouteChangeTracker from "./components/RouteChangeTracker";
 
 const App = () => {
+  const TRACKING_ID = "G-DKV7EG3Q54";
+  ReactGA.initialize(TRACKING_ID);
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/list" element={<ProductList />} />
-      </Routes>
+      <RouteChangeTracker>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/list" element={<ProductList />} />
+        </Routes>
+      </RouteChangeTracker>
       <Footer />
     </>
   );
